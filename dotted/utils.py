d6fd978f17fcd31539cba24eadaeb7b63b44dd3f
Carlos Escribano <carlosescri@gmail.com>
2015-07-20 13:59:38 +0200

    qf
    Signed-off-by: Carlos Escribano <carlosescri@gmail.com>

--- dotted/utils.py
+++ dotted/utils.py
@@ -4,8 +4,10 @@ from dotted.collection import DottedCollection
 
 
 def dot(value):
+    """Converts a value into a DottedCollection"""
     return DottedCollection.factory(value)
 
 
 def dot_json(json_value):
+    """Creates a DottedCollection from a JSON string"""
     return DottedCollection.load_json(json_value)
